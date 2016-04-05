1781ae2537bdd818535a759e6128d0cfa8738d4b
Valentin Kulichenko <valentin.kulichenko@gmail.com>
2016-04-05 16:50:11 -0700

    many-clients-test

--- start-server.sh
+++ start-server.sh
@@ -15,8 +15,8 @@ fi
 
 export IGNITE_WORK_DIR=`pwd`/work
 
-rm -rf $IGNITE_WORK_DIR
-
 $JAVA -Xms4g -Xmx4g -XX:+HeapDumpOnOutOfMemoryError -Denv=$1 \
+    -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=49112 \
+    -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false \
     -DIGNITE_WORK_DIR=$IGNITE_WORK_DIR -DIGNITE_QUIET=false -DIGNITE_UPDATE_NOTIFIER=false \
     -cp target/many-clients-test-1.0-SNAPSHOT-jar-with-dependencies.jar Server
