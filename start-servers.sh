cd7b5c56631ffa525991329d04f83c99a382b7ba
isuntsov-gridgain <isuntsov@gridgain.com>
2016-04-28 13:34:42 +0400

    Update start-servers.sh

--- start-servers.sh
+++ start-servers.sh
@@ -22,6 +22,8 @@ rm -rf $IGNITE_WORK_DIR
 for ((i=1;i<=$2;i++))
 do
     $JAVA -Xms2g -Xmx4g -XX:+HeapDumpOnOutOfMemoryError -Denv=$1 \
+        -Xloggc:./gc${now0}.log -XX:+PrintGCDetails \
+        -verbose:gc -XX:+UseParNewGC -XX:+UseConcMarkSweepGC \
         -DIGNITE_WORK_DIR=$IGNITE_WORK_DIR -DIGNITE_UPDATE_NOTIFIER=false \
         -cp target/many-clients-test-1.0-SNAPSHOT-jar-with-dependencies.jar Server > /dev/null &
 done
