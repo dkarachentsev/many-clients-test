f75abf0f285f8825b7a5c45bbe1e6a4fa71ef114
dkarachentsev <dkarachentsev@gridgain.com>
2016-11-07 17:51:10 +0300

    Enable JFR

--- start-servers.sh
+++ start-servers.sh
@@ -27,6 +27,7 @@ for ((i=1;i<=$2;i++))
 do
     $JAVA -Xms2g -Xmx4g -XX:+HeapDumpOnOutOfMemoryError -Denv=$1 \
         -XX:+UnlockCommercialFeatures -XX:+FlightRecorder \
+        -XX:StartFlightRecording=duration=120s,delay=210s,filename=$IGNITE_WORK_DIR/server.jfr \
         -Xloggc:./gc${i}.log -XX:+PrintGCDetails \
         -verbose:gc -XX:+UseParNewGC -XX:+UseConcMarkSweepGC \
         -DIGNITE_WORK_DIR=$IGNITE_WORK_DIR -DIGNITE_UPDATE_NOTIFIER=false \
