d6df26754fe1e5b3a34d0d649c42d4f67469eb05
dkarachentsev <dkarachentsev@gridgain.com>
2016-11-21 23:23:41 +0300

    gc

--- start-servers.sh
+++ start-servers.sh
@@ -27,7 +27,7 @@ for ((i=1;i<=$2;i++))
 do
     $JAVA -Xms2g -Xmx4g -XX:+HeapDumpOnOutOfMemoryError -Denv=$1 \
         -XX:+UnlockCommercialFeatures -XX:+FlightRecorder \
-        -Xloggc:./gc${i}.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps \
+        -Xloggc:./gc${i}.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime \
         -verbose:gc -XX:+UseParNewGC -XX:+UseConcMarkSweepGC \
         -DIGNITE_WORK_DIR=$IGNITE_WORK_DIR -DIGNITE_UPDATE_NOTIFIER=false \
         -cp target/many-clients-test-1.0-SNAPSHOT-jar-with-dependencies.jar Server > /dev/null &
